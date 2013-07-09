7306c6e49817737e40c05a5ae03247b377bbc5ed
Mike Huot <mhuot@opennms.og>
2013-07-09 15:52:15 -0500

    Improved support for customized settings for RedHat based systems, the default options were also corrected

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.2.0:
+
+* Improved support for customized settings for RedHat based systems, the default options were also corrected
+
 ## v1.1.0:
 
 * [COOK-1849] - Add RHEL support
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "matt@opscode.com"
 license          "Apache 2.0"
 description      "Installs/Configures tftpd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.1.0"
+version          "1.2.0"
 
 %w{ debian ubuntu fedora scientific centos redhat}.each do |os|
   supports os
