4f92837c18ffc40038684b63279ddd5a244fee82
Tim Smith <tsmith84@gmail.com>
2015-09-08 12:35:42 -0700

    Add oracle, update maintainer e-mail add supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -1,14 +1,18 @@
 name             "tftp"
 maintainer       "Chef Software, Inc."
-maintainer_email "matt@chef.io"
+maintainer_email "cookbooks@chef.io"
 license          "Apache 2.0"
 description      "Installs/Configures tftpd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.2.0"
 
-%w{ debian ubuntu fedora scientific centos redhat}.each do |os|
+%w{ debian ubuntu fedora scientific centos redhat oracle}.each do |os|
   supports os
 end
+
+source_url 'https://github.com/chef-cookbooks/tftp' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/tftp/issues' if respond_to?(:issues_url)
+
 
 attribute "tftp/username",
   :display_name => "tftp user",
