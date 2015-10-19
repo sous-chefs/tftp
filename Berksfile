0b75744c9b8ce46405e71991c03f6a6c260fd6da
Tim Smith <tsmith84@gmail.com>
2015-10-19 14:41:29 -0700

    Remove old minitest cookbook that didn't work

--- Berksfile
+++ Berksfile
@@ -3,7 +3,5 @@ source 'https://supermarket.chef.io'
 metadata
 
 group :integration do
-  cookbook 'minitest-handler'
   cookbook 'apt'
-  cookbook 'tftp_test', path: './test/cookbooks/tftp_test'
 end
--- test/cookbooks/tftp_test/metadata.rb
+++
@@ -1,8 +0,0 @@
-name             'tftp_test'
-maintainer       'Chef Software, Inc.'
-maintainer_email 'cookbooks@chef.io'
-license          'Apache 2.0'
-description      'This cookbook is used with test-kitchen to test the parent, tftp cookbook.'
-version          '1.0.0'
-
-depends 'tftp'
