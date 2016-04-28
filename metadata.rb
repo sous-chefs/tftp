224e2323f4abe3c0949b470a14beba5866fb305a
Tim Smith <tsmith84@gmail.com>
2016-04-28 16:59:31 -0700

    Testing updates

--- metadata.rb
+++ metadata.rb
@@ -12,24 +12,3 @@ end
 
 source_url 'https://github.com/chef-cookbooks/tftp' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/tftp/issues' if respond_to?(:issues_url)
-
-attribute 'tftp/username',
-  display_name: 'tftp user',
-  default: 'tftp'
-
-attribute 'tftp/directory',
-  display_name: 'tftp directory',
-  description: 'Directory to be shared via tftp.',
-  default: '/var/lib/tftpboot'
-
-attribute 'tftp/address',
-  display_name: 'tftp address',
-  default: '0.0.0.0:69'
-
-attribute 'tftp/tftp_options',
-  display_name: 'tftp tftp_options',
-  default: '--secure'
-
-attribute 'tftp/options',
-  display_name: 'tftp options',
-  default: '-s'
