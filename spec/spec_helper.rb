a57d96da8535bd8a08962a7a6fe0e3a71d3a878b
Tim Smith <tsmith@chef.io>
2016-05-19 17:26:31 -0700

    Remove serverspec tests and chefspec report
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,3 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
 
-at_exit { ChefSpec::Coverage.report! }
--- test/integration/default/serverspec/default/tftp_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper'
-
-describe port(69) do
-  it { should be_listening }
-end
