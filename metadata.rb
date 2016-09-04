efdf292ed1dc15b7db5b5f99c918ee53ceb52c0a
Tim Smith <tsmith@chef.io>
2016-09-03 22:24:14 -0700

    Require Chef 12 + travis updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -10,11 +10,9 @@ version          '2.0.0'
   supports os
 end
 
-%w(xinetd).each do |ck|
-  depends ck
-end
+depends 'xinetd'
 
-source_url 'https://github.com/chef-cookbooks/tftp' if respond_to?(:source_url)
-issues_url 'https://github.com/chef-cookbooks/tftp/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/chef-cookbooks/tftp'
+issues_url 'https://github.com/chef-cookbooks/tftp/issues'
 
-chef_version '>= 11' if respond_to?(:chef_version)
+chef_version '>= 12'
