67f1ae5d58a9d5bd4920dca48c43328dd7663afa
Tim Smith <tsmith@chef.io>
2017-02-12 07:10:02 -0800

    Testing updates
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -14,5 +14,4 @@ depends 'xinetd'
 
 source_url 'https://github.com/chef-cookbooks/tftp'
 issues_url 'https://github.com/chef-cookbooks/tftp/issues'
-
-chef_version '>= 12.1'
+chef_version '>= 12.1' if respond_to?(:chef_version)
