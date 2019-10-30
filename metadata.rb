8966845b8b6a36a5958788633e9b7c2280ec244d
Tim Smith <tsmith84@gmail.com>
2019-10-30 14:08:47 -0700

    Cookstyle fixes
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer       'Chef Software, Inc.'
 maintainer_email 'cookbooks@chef.io'
 license          'Apache-2.0'
 description      'Installs/Configures tftpd'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.0.1'
 
 %w( amazon debian ubuntu fedora scientific centos redhat oracle).each do |os|
